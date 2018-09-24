cdb7e8622ab7fa8a604fc4eca45e2ff59ef3a3a3
ice <ice@lab06m05.nrc.ice.ufjf.br>
2018-09-24 19:47:22 -0300

    Organizando projeto java

---
+++ Pokemon/src/pokemon/Pokemon.java
@@ -0,0 +1,13 @@
+
+public class Pokemon {
+	String nome;
+	String tipo;
+	int evolucao;
+	String habilidade1;
+	String habilidade2;
+	String habilidade3;
+	int manaPoints;
+	int healthPoints;
+	int strength;
+	int armorClass;
+}
