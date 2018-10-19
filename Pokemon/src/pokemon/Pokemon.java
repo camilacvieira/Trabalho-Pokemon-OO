642caf8f4368d125570ab1e15c3603a11d9a24ed
Arthur Dornelas <arthurdornelas99@gmail.com>
2018-10-19 16:28:53 -0300

    Add files via upload

---
+++ Pokemon/src/pokemon/Pokemon.java
@@ -0,0 +1,24 @@
+package pokemon;
+import java.util.ArrayList;
+
+public abstract class Pokemon {
+	
+        String nome;
+	String tipo1;
+        String tipo2;
+	int evolucao;
+	String habilidade1;
+	String habilidade2;
+	String habilidade3;
+	int manaPoints;
+	int healthPoints;
+	int strength;
+	int armorClass;
+        ArrayList vantagens;
+        ArrayList fraquesas;
+
+    
+        
+        
+        
+}
