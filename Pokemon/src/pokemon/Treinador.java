1dc981e3229a16f8414b4026c8105564bec66b6c
ArthurDornelas <arthurdornelas99@gmail.com>
2018-11-27 20:22:53 -0200

    Implementação treinador

--- Pokemon/src/pokemon/Treinador.java
+++ Pokemon/src/pokemon/Treinador.java
@@ -5,10 +5,33 @@
  */
 package pokemon;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Arthur
  */
 public class Treinador {
+    private ArrayList<Pokemon> time;
+	
+	public Treinador(Pokemon pokemon) {
+		time = new ArrayList<Pokemon>();
+		time.add(pokemon);
+	}
+	
+	public void addPokemon(Pokemon pokemon) {
+		
+			time.add(pokemon);          
+	}
+	
+	public Pokemon getPokemon(int index) {
+		return time.get(index);
+	}
+	
+	public int getTimeSize() {
+		return time.size();
+	}
+
     
 }
