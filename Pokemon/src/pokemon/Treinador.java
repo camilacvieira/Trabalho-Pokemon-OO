2b03e349fcb8090922098850264dc2f9298617d2
ArthurDornelas <arthurdornelas99@gmail.com>
2018-11-30 01:17:08 -0200

    Implementando Time

--- Pokemon/src/pokemon/Treinador.java
+++ Pokemon/src/pokemon/Treinador.java
@@ -13,25 +13,18 @@ import java.util.List;
  * @author Arthur
  */
 public class Treinador {
-    private ArrayList<Pokemon> time;
+    private float vidaTotal;    
+    private Time t = new Time();
 	
-	public Treinador(Pokemon pokemon) {
-		time = new ArrayList<Pokemon>();
-		time.add(pokemon);
-	}
-	
-	public void addPokemon(Pokemon pokemon) {
+	public Treinador(Time t) {
+            this.t = t;
+            vidaTotal = t.pokemons.get(0).healthPoints + t.pokemons.get(1).healthPoints + t.pokemons.get(2).healthPoints;
 		
-			time.add(pokemon);          
 	}
+        
+        public void calculaVida(){
+            vidaTotal = t.pokemons.get(0).healthPoints + t.pokemons.get(1).healthPoints + t.pokemons.get(2).healthPoints;
+        }
 	
-	public Pokemon getPokemon(int index) {
-		return time.get(index);
-	}
-	
-	public int getTimeSize() {
-		return time.size();
-	}
-
     
 }
