c04af947758c46e713fbfacca8f3d3d3108f480c
ArthurDornelas <arthurdornelas99@gmail.com>
2018-11-30 01:02:54 -0200

    Implementando Time

--- Pokemon/src/pokemon/Pokemon.java
+++ Pokemon/src/pokemon/Pokemon.java
@@ -9,18 +9,20 @@ public class Pokemon {
         public int tipo; // 0-Agua ; 1-Eletrico ; 2-Fogo ; 3-Normal ; 4-Pedra ; 5-Planta ; 6-Psiquico
         public int evolucao;
 	public String[] habilidades = new String[3];
-        public float[] multiHabilidades = {1.5f,1.2f,1};
+        public float[] multiHabilidades = {2,1.5f,1};
 	public int manaPoints;
 	public int healthPoints;
-	public int strength = 100;
+	public float dano = 10;
 	public int armorClass;
        
         // 0-Agua ; 1-Eletrico ; 2-Fogo ; 3-Normal ; 4-Pedra ; 5-Planta ; 6-Psiquico
         public int[] vantagens; 
         public int[] fraquesas;
+        
+        public void calculaAtaque(int index){
+            dano = 100;
+            dano = dano * multiHabilidades[index];
+        }
 
-    
-        
-        
-        
+
 }
